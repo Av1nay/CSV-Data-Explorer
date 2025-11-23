806510a73f20d9ed2efdd4ee429ad6f65f1681ac
Av1nay <avi9071@gmail.com>
2025-11-23 23:52:23 +0200

    Add delimiter detection and improve extension validation

--- data_cleaner/loaders/loader.py
+++ data_cleaner/loaders/loader.py
@@ -1,4 +1,5 @@
-import os 
+import os
+import csv
 
 def get_file_path():
     # ask user for file path and remove any leading/trailing whitespace
@@ -23,19 +24,31 @@ def validate_file_extension(file_path):
     _, extension = os.path.splitext(file_path)
     extension = extension.lower() # normalize the extensions
 
-    valid_extensions = [".csv", ".xlsx", "xls"]
+    valid_extensions = [".csv", ".xlsx", ".xls"]
 
     if extension not in valid_extensions:
         print(f"Unsuported file type {extension}")
-        print(f"Supported formats: {extension}")
+        print(f"Supported formats: {',' .join(extension)}") # shows in comma (,) separated not in tuple
         return None
     
-    print(f"File extension detected: {extension}")
     return extension
 
+# detecting delimiter
+def detect_delimiter(file_path):
+    try:
+        with open(file_path, 'r', newline='', encoding='utf-8') as f:
+            sample = f.read(2048) # read only few lines (only 2 Kb)
+            delimiter = csv.Sniffer().sniff(sample).delimiter
+            print(f"Detected delimiter: {delimiter}")
+            return delimiter
+    except Exception as e:
+        print(f"Counld not delimiter: {delimiter}", e)
+        return None
+
 # test
-if __name__ == "__main__":
+if __name__ == "__main__": # run this code only when executed directly
     path = get_file_path()
     if path:
         extension = validate_file_extension(path)
-        print("File extension is:", extension)
+        if extension == ".csv":
+            delimiter = detect_delimiter(path)
