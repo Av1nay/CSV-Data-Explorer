44c168c9116f3e07e20f13889141c1192eaceff6
Av1nay <avi9071@gmail.com>
2025-11-23 23:26:15 +0200

    Add file extension validation to loader

--- data_cleaner/loaders/loader.py
+++ data_cleaner/loaders/loader.py
@@ -18,5 +18,24 @@ def get_file_path():
 
     return file_path
 
-path = get_file_path()
-print("File path is:", path)
+# validating file extension
+def validate_file_extension(file_path):
+    _, extension = os.path.splitext(file_path)
+    extension = extension.lower() # normalize the extensions
+
+    valid_extensions = [".csv", ".xlsx", "xls"]
+
+    if extension not in valid_extensions:
+        print(f"Unsuported file type {extension}")
+        print(f"Supported formats: {extension}")
+        return None
+    
+    print(f"File extension detected: {extension}")
+    return extension
+
+# test
+if __name__ == "__main__":
+    path = get_file_path()
+    if path:
+        extension = validate_file_extension(path)
+        print("File extension is:", extension)
