b8897f66192e6864049063cbf74c0baa82fac42c
Av1nay <avi9071@gmail.com>
2025-11-26 00:14:55 +0200

    Implement duplicate checker, refine structure check, and update test data

---
+++ data_cleaner/inspectors/duplicate_checker.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+
+def detect_dupliates(df):
+    report = {}
+
+    exact_duplicates = df[
+        df.duplicated(keep=False)
+    ]  # marks every row that is part of duplicates - including first occurance
+
+    report["number_of_exact_duplicates"] = exact_duplicates.shape[
+        0
+    ]  # exact number of duplicates rows
+    report["exact_duplicates_rows"] = exact_duplicates.head(5).to_dict(
+        "records"
+    )  # first 5 duplicate rows only (for display) and convert rows into list-of-dictionaries format
+
+    # Check for partial duplicates (normalize basic formatting)
+    df_normalize = df.copy()  # copy df to normalize. do not modify original df.and
+
+    # normalize text columns (strip spaces, lower)
+    for col in df_normalize.select_dtypes(include="object").columns:
+        df_normalize[col] = df_normalize[col].astype(str).str.strip().str.lower()
+
+    partial_duplicates = df_normalize[df_normalize.duplicated(keep=False)]
+    report["number_of_partial_duplicates"] = partial_duplicates.shape[0]
+
+    return report
+
+
+# ----------------------
+# remove duplicates
+def remove_duplicates(df):
+    cleaned_df = df.drop_duplicate()
+    return cleaned_df
--- data_cleaner/main.py
+++ data_cleaner/main.py
@@ -1,6 +1,8 @@
+from inspectors.duplicate_checker import detect_dupliates, remove_duplicates
 from inspectors.structure_checker import check_structure
 from loaders.loader import *
 
+# load file
 path = get_file_path()
 extension = validate_file_extension(path)
 
@@ -10,7 +12,26 @@ if extension == ".csv":
     df = load_data(path, extension, delimiter, encoding)
 else:
     df = load_data(path, extension)
-
+# check structure
 structure_report = check_structure(df)
 print("\n Structure report:")
-print(structure_report)
+# print(structure_report)
+for key, value in structure_report.items():
+    print(f"{key}: {value}")
+
+# check duplicates
+for key, value in detect_dupliates(df).items():
+    print(f"{key} : {value}")
+
+# option to remove or keep duplicates
+choice = (
+    input("\nDo you want to remmove duplicates, if yes press 'y' for no press 'n: ")
+    .strip()
+    .lower()
+)
+
+if choice == "y":
+    df = remove_duplicates(df)
+    print("Exact duplicates removed")
+else:
+    print("Exact duplicates not removed")
